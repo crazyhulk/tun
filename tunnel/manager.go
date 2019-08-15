35cff79eec9226182628ce20ceae1744824f9bf4
zhuxi <732632754@qq.com>
2019-08-15 22:57:13 +0800

    release tun

--- tunnel/manager.go
+++ tunnel/manager.go
@@ -100,7 +100,7 @@ func tunToTcp(conn *net.TCPConn, tun *water.Interface) (err error) {
 		binary.LittleEndian.PutUint32(headerBuf, uint32(n))
 		fmt.Println(headerBuf)
 		count, err := conn.Write(headerBuf)
-		if err != io.EOF {
+		if err != nil {
 			tun.Close()
 			conn = nil
 			tun = nil
@@ -111,7 +111,7 @@ func tunToTcp(conn *net.TCPConn, tun *water.Interface) (err error) {
 
 		count, err = conn.Write(packets)
 		fmt.Println("write conn:", count)
-		if err != io.EOF {
+		if err != nil {
 			tun.Close()
 			conn = nil
 			tun = nil
